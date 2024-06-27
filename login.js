af49ae71db0654f039afa309ec5b31fae68ccb5f
krishna-madhaparia <krishina.madhaparia@strathmore.edu>
2024-06-27 17:03:03 +0300

    Added login javascrpit

---
+++ login.js
@@ -0,0 +1,55 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYN8IWC2NaSN5nJfCxKEwXN79OWZDKqpw",
+    authDomain: "webapp-b4d18.firebaseapp.com",
+    databaseURL: "https://webapp-b4d18-default-rtdb.firebaseio.com",
+    projectId: "webapp-b4d18",
+    storageBucket: "webapp-b4d18.appspot.com",
+    messagingSenderId: "540649378754",
+    appId: "1:540649378754:web:417750dd3fa64d07473c5b",
+    measurementId: "G-3QZ85W69MN"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference your database
+var EventManagementDB = firebase.database().ref("EventManagement");
+
+document.getElementById("loginForm").addEventListener("submit", login);
+
+function login(e) {
+    e.preventDefault();
+    var emailid = getElementVal("login_emailid");
+    var password = getElementVal("login_password");
+
+    var user_ref = EventManagementDB.orderByChild("emailid").equalTo(emailid);
+    user_ref.once("value", function(snapshot) {
+        if (snapshot.exists()) {
+            let loggedIn = false;
+            snapshot.forEach(function(childSnapshot) {
+                var user = childSnapshot.val();
+                if (user.password === password) {
+                    loggedIn = true;
+                }
+            });
+            if (loggedIn) {
+                // Redirect to FAQ.html after successful login
+                window.location.href = "FAQ.html";
+            } else {
+                displayError("Login failed. Please check your credentials.");
+            }
+        } else {
+            displayError("Login failed. User not found.");
+        }
+    });
+}
+
+function displayError(message) {
+    var errorBox = document.getElementById("loginError");
+    errorBox.innerText = message;
+    errorBox.style.display = "block";
+}
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+};
